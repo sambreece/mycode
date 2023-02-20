f518a395e74fc609cc4820f1c67841c8fecbb242
sambreece <samantha.breece@tlgcohort.com>
2023-02-20 22:18:12 +0000

    input challenge script

---
+++ input_challenge.py
@@ -0,0 +1,8 @@
+ #!/usr/bin/env python3
+
+#Take user input
+name = input("Enter your name: ")
+day = input("Enter the day of the week: ")
+
+#print statement including inputs
+print(f"Hello, {name}! Happy {day}!")
