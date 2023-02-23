0e8f21918f40a75d97e207fe3bbcbe90eeecc7be
sambreece <samantha.breece@tlgcohort.com>
2023-02-23 17:08:31 +0000

    completed basic structure of main

--- my_projects/week1/week1_project.py
+++ my_projects/week1/week1_project.py
@@ -25,34 +25,44 @@ def explain_rules():
     print(f"To win this game, you must determine the mystery dish by making guesses about its properties.\nAfter {max_guesses} opportunities to gather information, you must guess the mystery dish in one try. ")
     
     #return if user wants to play
-    return True if input("Are you up for the challenge?(yes/no) \n>").lower() == "yes" else False
+    return True if input("Are you up for the challenge?(yes/no) \n> ").lower() == "yes" else False
 
 def guess_properties(mystery_dish): 
     guesses = 0
     while guesses < max_guesses:
         guesses += 1
-        print(f"Current guess: {guesses}")
+        print(f"Current guess: {guesses}.")
     
-        # What property of the food would you like to guess? (type(2), main ingredients(4), colors(3), country of origin(1))
+        # What property of the food would you like to guess? (type(2), ingredients(4), colors(3), country of origin(1))
         while True:
             #update this to have properties and their quantity come from a list
             property = input(f"What propery of the dish would you like to guess? \
                 \nOptions: \
-                \nType, ingredients, colors, temperature when served, country of origin \n> ")
+                \nType, ingredients, colors, temperature when served, country of origin \n> ").lower()
     
             #validate the user's input is from the list, otherwise tell them it's invalid and make them choose again
-                #if property in list of properties, break 
+            #if property in list of properties, break 
+            if property in mystery_dish.keys():
+                # let them guess property
+                property_guess = input(f"Guess about the dishes' {property}.\n> ").lower()
+                if property_guess in mystery_dish[property]:
+                    print("Correct!")
+                else:
+                    print("Wrong!")
+                break
+            else: 
+                print("You can't guess about that property!")
         
-        #handle guessing property, decide how those output strings should look
-        #should i let them duplicate guesses? decide this
+            #handle guessing property, decide how those output strings should look
+            #should i let them duplicate guesses? decide this
         
         #print remaining number of guesses
-        
+        print(f"Guesses remaining: {max_guesses - guesses}.")
             
 
 def guess_mystery_dish(mystery_dish):
     #let user guess the mystery dish
-    final_guess = input("The time has come. Guess the mystery food. \n>").lower()
+    final_guess = input("The time has come. Guess the mystery food. \n> ").lower()
     
     #check answer
     if final_guess == mystery_dish:
@@ -62,7 +72,7 @@ def guess_mystery_dish(mystery_dish):
         Days later, it comes to you in a dream. It was {mystery_dish['name']}.")
     
     #return if user wants to play again
-    return True if input("Do you want to play again?(yes/no) \n>").lower() == "yes" else False   
+    return True if input("Do you want to play again?(yes/no) \n> ").lower() == "yes" else False   
 
 if __name__ == "__main__":
     main()   
