184c2b010274a5b97d5ab52b4d85af0e1b87a15f
sambreece <samantha.breece@tlgcohort.com>
2023-02-28 23:13:53 +0000

    exploring how to work with large data sets

--- my_projects/week2/week2_project.py
+++ my_projects/week2/week2_project.py
@@ -1,2 +1,14 @@
 #!usr/bin/env python3
+import TermTk as ttk
 
+    # Create a root object (it is a widget that represent the terminal)
+root = ttk.TTk()
+
+    # Create a window and attach it to the root (parent=root)
+helloWin = ttk.TTkWindow(parent=root,pos = (1,1), size=(30,10), title="Hello Window", border=True)
+
+    # Define the Label and attach it to the window (parent=helloWin)
+ttk.TTkLabel(parent=helloWin, pos=(5,5), text="Hello World")
+
+    # Start the Main loop
+root.mainloop()
