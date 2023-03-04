5591f1a3e23b2697a2a0e6d1401d626bc9c9206e
sambreece <samantha.breece@tlgcohort.com>
2023-03-04 00:24:42 +0000

    basic project function complete

--- my_projects/week2/week2_project.py
+++ my_projects/week2/week2_project.py
@@ -27,7 +27,20 @@ class QuestionPage(Screen):
                 ["Which aisle should you use to exit the grocery store?", "A. Frozen food aisle. It seems quiet.",
                     "B. Vegetable aisle. There are only one or two shoppers, minding their own business.",
                     "C. Snack aisle. It seems busy, but maybe that will cover your tracks.",
-                    "D. Beverage aisle. Do lobsers like soda?"]
+                    "D. Beverage aisle. Do lobsers like soda?"],
+                None,
+                None,
+                None, 
+                ["How should you get to the ocean?","A. Walk the shortest path", 
+                    "B. Walk the most secluded path", 
+                    "C. Ask the lobster", 
+                    "D. Take the bus" ],
+                None, 
+                None, 
+                ["How will you get to the water?", "A. Walk slowly so no one is suspicious",
+                    "B. Sprint across the sand",
+                    "C. Walk at normal pace",
+                    "D. Release the lobster here"]
                 ]
         self.styles.background = "#4682B4"
         self.styles.text_align = "center"
@@ -56,7 +69,15 @@ class TextPage(Screen):
                 None,
                 "You made it out of the grocery store!  You hear a happy noise from inside your inventory.",
                 "You can see the glistening waves of the ocean in the distance.",
-                "🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊"
+                "🌊🌊🌊🌊🌊",
+                None,
+                "The lobster seems to like the bus.",
+                "You step off the bus when it gets to the beach. Now you just have to get to the water!", 
+                None,
+                "As you walk toward the water, the lobster gets more restless. It seems to know it's close to the ocean.",
+                "You release the lobster into the water and watch it swim away. Bye, friend!",
+                "🌊🦞 🌊🌊",
+                "You win the game! \nPress 'q' to quit."
                 ]
         self.styles.background = "#4682B4"
         self.styles.align_vertical = "middle"
@@ -138,6 +159,14 @@ class GameOverPage(Screen):
         if event.button.id == "return":
             self.app.pop_screen()
 
+class GoodByePage(Screen):
+
+    def compose(self) -> ComposeResult:
+        self.styles.align_vertical = "middle"
+        self.box = Static("Thanks for playing! \nPlease use 'q' to quit the program")
+        self.box.styles.text_align = "center"
+        yield self.box
+
 #Main application:
 class LobsterLiberatorApp(App):
     BINDINGS = [
@@ -157,13 +186,18 @@ class LobsterLiberatorApp(App):
         
     async def on_key(self, event: events.Key) -> None:
         screens = [TextPage(self.counter), TextPage(self.counter), TextPage(self.counter), TextPage(self.counter), QuestionPage(self.counter),
-                None, QuestionPage(self.counter), TextPage(self.counter), TextPage(self.counter)]
-        correct_answer = [None, None, None, None, "b", None, "c"]
+                None, QuestionPage(self.counter), None, TextPage(self.counter), TextPage(self.counter), QuestionPage(self.counter),
+                None,TextPage(self.counter), QuestionPage(self.counter), None, TextPage(self.counter), TextPage(self.counter), TextPage(self.counter), GoodByePage() ]
+        correct_answer = [None, None, None, None, "b", None, "c", None, None, None, "d", None, None, "a", None, None]
         if event.key == "enter":
-            self.app.push_screen(screens[self.counter])
-            self.counter += 1
+            if self.counter > 17:
+                self.app.exit()
+            else:
+                self.app.push_screen(screens[self.counter])
+                self.counter += 1
         if event.key == "a":
-            response_a = [ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage()]
+            response_a = [ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage(),
+                    ErrorPage(), ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),TextPage(self.counter)]
             if "a" == correct_answer[self.counter-1]:
                 self.app.push_screen(response_a[self.counter])
                 self.counter+=1
@@ -179,26 +213,32 @@ class LobsterLiberatorApp(App):
             else:
                 self.app.push_screen(GameOverPage("Someone spotted a lobster claw hanging out of your bag."))
         if event.key == "c":
-            response_c = [ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(), TextPage(self.counter),ErrorPage()]
+            response_c = [ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage(), 
+                    ErrorPage(), TextPage(self.counter),ErrorPage(), ErrorPage(),ErrorPage(),TextPage(self.counter), ErrorPage()]
             if "c" == correct_answer[self.counter-1]:
                 self.app.push_screen(response_c[self.counter])
                 self.counter+=1
             elif self.counter-1 == 4:
                 self.app.push_screen(AnotherChancePage("The lobster easily avoids the tongs."))
+            elif self.counter-1 == 10:
+                self.app.push_screen(AnotherChancePage("The lobster seems confused by the question."))
             else:
                 self.app.push_screen(GameOverPage("Someone spotted a lobster claw hanging out of your bag."))
         if event.key == "d":
-            response_d = [ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage()]
+            response_d = [ErrorPage(),ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(),ErrorPage(),
+                    ErrorPage(), ErrorPage(),ErrorPage(), ErrorPage(),ErrorPage(), TextPage(self.counter)]
             if "d" == correct_answer[self.counter-1]:
                 self.app.push_screen(response_d[self.counter])
                 self.counter+=1
             elif self.counter-1 == 4:
                 self.app.push_screen(AnotherChancePage("The lobster doesn't seem interested in the pen."))
+            elif self.counter-1 == 13:               
+                self.app.push_screen(AnotherChancePage("You put the lobster down on the sand, but it seems to follow you, so you decide to pick it back up."))
             else:
                 self.app.push_screen(GameOverPage("Someone spotted a lobster claw hanging out of your bag."))
         if event.key == "i":
             inventory = ["pen", "wired headphones"]
-            if self.counter > 4:
+            if self.counter > 4 and self.counter < 15:
                 inventory.append("lobster")
             self.app.push_screen(InventoryPage(inventory))
 if __name__ == "__main__":
